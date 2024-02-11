1d19eab724e8927efe34ea4f6eba8e56beb69a4f
Abdo Mahmoud <UniqDigiR@gmail.com>
2024-02-11 20:30:46 +0200

    Update button.rs
    Apache 2.0 copyright added

--- src/uwidgets/button.rs
+++ src/uwidgets/button.rs
@@ -1,3 +1,11 @@
+// NOTES:
+//==============================================================================
+// written by Abdo Mahmoud (Unique-Digital-Resources): https://github.com/Unique-Digital-Resources
+// Copyright 2024 Abdo Mahmoud
+// Apache 2.0
+//==============================================================================
+
+
 pub struct Button <T: ?Sized>{
     pub disable: bool,
     pub on_hover: Box<dyn Fn(&T)>,
