60837f973a6ecbaf2cddea3cf4df83f7841e33e0
Abdo Mahmoud <UniqDigiR@gmail.com>
2024-02-11 20:29:47 +0200

    Update uwidgetype.rs
    Apache 2.0 copyright added

--- src/uwidgets/uwidgetype.rs
+++ src/uwidgets/uwidgetype.rs
@@ -1,3 +1,10 @@
+// NOTES:
+//==============================================================================
+// written by Abdo Mahmoud (Unique-Digital-Resources): https://github.com/Unique-Digital-Resources
+// Copyright 2024 Abdo Mahmoud
+// Apache 2.0
+//==============================================================================
+
 use crate::uwidgets::button::Button;
 use std::any::Any;
 
